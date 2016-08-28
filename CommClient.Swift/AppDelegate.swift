027cb2bfb01129ddf12623baddb1a27540f03557
Andrew M. Bray <andrew@andrews-imac.local>
2016-08-28 21:40:22 +0900

    Initial Commit

---
+++ CommClient.Swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CommClient.Swift
+//
+//  Created by Andrew M. Bray on 2016. 8. 28..
+//  Copyright © 2016년 Andrew M. Bray. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CommClient.Swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CommClient.Swift
+//
+//  Created by Andrew M. Bray on 2016. 8. 28..
+//  Copyright © 2016년 Andrew M. Bray. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
